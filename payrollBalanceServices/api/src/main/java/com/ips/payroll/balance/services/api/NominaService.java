ebd9e68b56b60a58674e5541874c73ea3f2ecaff
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-06-23 22:53:14 -0500

    Initial Draft

---
+++ payrollBalanceServices/api/src/main/java/com/ips/payroll/balance/services/api/NominaService.java
@@ -0,0 +1,9 @@
+package com.ips.payroll.balance.services.api;
+
+/**
+ * Created by acardenas on 6/23/14.
+ */
+public interface NominaService
+{
+
+}
---
+++ payrollBalanceServices/ejb/src/main/java/com/ips/payroll/balance/services/ejb/NominaServiceEJB.java
@@ -0,0 +1,9 @@
+package com.ips.payroll.balance.services.ejb;
+
+/**
+ * Created by acardenas on 6/23/14.
+ */
+@Stateless
+public class NominaServiceEJB
+{
+}
